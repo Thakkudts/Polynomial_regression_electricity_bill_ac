f109875926cd93efcb52d759708bf91053404e02
Devangana T S <148966769+Thakkudts@users.noreply.github.com>
2026-09-18 08:59:23 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import joblib
+
+model = joblib.load("polynomial_regression_model.pkl")
+
+st.title("⚡ AC Units vs Electric Bill")
+st.write("Predict Electric Bill using Polynomial Regression")
+
+ac_units = st.number_input(
+    "Enter AC Electricity Consumption (Units):",
+    min_value=0.0,
+    value=100.0,
+    step=1.0
+)
+
+if st.button("Predict Electric Bill"):
+
+    prediction = model.predict([[ac_units]])
+
+    st.success(f"Expected Electric Bill: ₹{prediction[0]:.2f}")
+
